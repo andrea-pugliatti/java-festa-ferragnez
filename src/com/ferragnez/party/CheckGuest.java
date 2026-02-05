8a759b421f66db84db86152040ca7fb07e51e6ff
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-05 18:43:38 +0100

    add logic using for loop

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,8 +1,33 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
-        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
-                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+        String[] guests = {
+                "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio",
+                "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"
+        };
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome: ");
+        String input = in.nextLine();
+        in.close();
+
+        boolean found = false;
+
+        for (int i = 0; i < guests.length; i++) {
+            if (guests[i].equals(input)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Puoi entrare!");
+        } else {
+            System.out.println("Vai via!");
+        }
+
     }
 }
